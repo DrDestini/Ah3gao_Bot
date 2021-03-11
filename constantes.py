74468ab18f2e4bbb48ec1673f3d8b99b4565a29c
DrDestini <80466071+DrDestini@users.noreply.github.com>
2021-03-11 14:35:22 +0100

    Delete constantes.py

--- constantes.py
+++
@@ -1,4 +0,0 @@
-
-
-TAGS_PELIGROSOS=["rape", "ugly bastard", "gore", "scat", "guro", "vomit", "vore", "tentacles", "golden shower", "bestiality", "bondage", "drugs", "pregnant", "stomach deformation"]
-
